7d184867726e13799ade1af40443917563d90ea2
kaibin.yang <kaibin.yang@ttpai.cn>
2021-03-19 12:00:06 +0800

    [format] compile 代码格式化

--- ttpai-mybatis-autoconfigure/ttpai-mybatis-autoconfigure-datasource/src/main/java/com/ttpai/framework/mybatis/autoconfigure/datasource/choose/package-info.java
+++ ttpai-mybatis-autoconfigure/ttpai-mybatis-autoconfigure-datasource/src/main/java/com/ttpai/framework/mybatis/autoconfigure/datasource/choose/package-info.java
@@ -2,9 +2,9 @@
  * 该包主要用于选择是否使用 MybatisAutoConfiguration
  *
  * @see com.ttpai.framework.mybatis.autoconfigure.datasource.choose.MyBatisAutoConfigurationExcludeFilter
- * 默认排除对 MybatisAutoConfiguration 的自动配置，后续会根据条件选择是否重新导入
- * <p>
+ *      默认排除对 MybatisAutoConfiguration 的自动配置，后续会根据条件选择是否重新导入
+ *      <p>
  * @see com.ttpai.framework.mybatis.autoconfigure.datasource.choose.MyBatisAutoConfigurationImporter
- * 如果 DataSource 的实例只有一个，则重新导入 MybatisAutoConfiguration
+ *      如果 DataSource 的实例只有一个，则重新导入 MybatisAutoConfiguration
  */
 package com.ttpai.framework.mybatis.autoconfigure.datasource.choose;
