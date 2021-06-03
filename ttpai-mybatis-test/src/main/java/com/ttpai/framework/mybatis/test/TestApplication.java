4d7d3591b84c2dd61c04c947429d8e3c1971cbbe
kaibin.yang <kaibin.yang@ttpai.cn>
2021-06-03 11:53:25 +0800

    fix(pagehelper): dependency

--- ttpai-mybatis-test/src/main/java/com/ttpai/framework/mybatis/test/TestApplication.java
+++ ttpai-mybatis-test/src/main/java/com/ttpai/framework/mybatis/test/TestApplication.java
@@ -3,6 +3,10 @@ package com.ttpai.framework.mybatis.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
 /**
  * @author lilin.tan@ttpai.cn
  * @since 2021/2/18 15:00
@@ -10,7 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final Enumeration<URL> systemResources = ClassLoader
+                .getSystemResources("META-INF/spring-autoconfigure-metadata.properties");
+        for (; systemResources.hasMoreElements();) {
+            System.out.println(systemResources.nextElement());
+        }
+
         SpringApplication.run(TestApplication.class, args);
     }
 
--- ttpai-mybatis-test/src/main/java/com/ttpai/framework/mybatis/test/TestRunner.java
+++ ttpai-mybatis-test/src/main/java/com/ttpai/framework/mybatis/test/TestRunner.java
@@ -1,5 +1,7 @@
 package com.ttpai.framework.mybatis.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ttpai.framework.mybatis.test.biz.student.dao.StudentMapper;
 import com.ttpai.framework.mybatis.test.biz.student.model.StudentVO;
 import com.ttpai.framework.mybatis.test.biz.user.dao.TtpaiUserMapper;
@@ -9,6 +11,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Component
 public class TestRunner implements ApplicationRunner {
@@ -26,6 +29,18 @@ public class TestRunner implements ApplicationRunner {
 
         StudentVO studentVO = studentMapper.selectById(1L);
         System.out.println(studentVO);
+
+        /* 查询所有 */
+        final List<StudentVO> studentAll = studentMapper.selectByEntity(new StudentVO());
+        System.out.println(studentAll.size());
+        System.out.println(studentAll);
+
+        /* 分页插件 */
+        final PageInfo<StudentVO> students = PageHelper.startPage(1, 2)
+                .doSelectPageInfo(() -> studentMapper.selectByEntity(new StudentVO()));
+
+        System.out.println(students.getTotal());
+        System.out.println(students.getList());
 
     }
 }
